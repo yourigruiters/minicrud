98aac9cfbd392415629cff2c5611d92d132c2776
Youri Gruiters <i-youri-i@hotmail.com>
2022-02-28 14:07:13 +0100

    Small changes - Committing for Uden

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 const nav = document.querySelector("nav");
 const introduction = document.querySelector(".introduction-spacer");
 
-$(window).scroll(function (e) {
+// Sticky navigation
+$(window).ready((e) => {
+  if (window.pageYOffset > introduction.offsetHeight) {
+    nav.classList.add("sticky");
+  }
+});
+
+$(window).scroll((e) => {
   console.log(introduction.offsetHeight);
   if (window.pageYOffset > introduction.offsetHeight) {
     nav.classList.add("sticky");
   } else {
     nav.classList.remove("sticky");
   }
+});
+
+const navList = document.querySelector("nav ul");
+
+// Navbar comes flying in
+$(window).ready((e) => {
+  navList.classList.add("appear");
 });
